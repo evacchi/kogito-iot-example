3f362363b7ab7b924b35c67d4caab8a6c57f1d93
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-10-09 10:52:00 +0200

    all boilerplate done

---
+++ src/main/java/org/kie/kogito/examples/iot/rules/ComputeAC.java
@@ -0,0 +1,12 @@
+package org.kie.kogito.examples.iot.rules;
+
+import org.kie.kogito.rules.DataSource;
+import org.kie.kogito.rules.DataStream;
+import org.kie.kogito.rules.RuleUnitMemory;
+
+public class ComputeAC implements RuleUnitMemory {
+    private DataStream<Object> updates = DataSource.createStream();
+    public DataStream<Object> getUpdates() {
+        return updates;
+    }
+}
